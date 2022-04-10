b82c41bfa62356f8e9102637eaa83103d7db7554
Falih Muqsith Setiyoko <45122509+FalihMS@users.noreply.github.com>
2022-04-10 15:49:10 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        fade: 'fadeOut 5s ease-in-out',
+      },
+      keyframes: theme => ({
+        fadeOut: {
+          '0%': { backgroundColor: theme('colors.red.300') },
+          '100%': { backgroundColor: theme('colors.transparent') },
+        },
+      }),
+    },
+  },
+  plugins: [],
+}
